36355121c55c19f81db2e228687c9c2c30a1493c
VladScv <ddcllrnsbrv@uoc.edu>
2022-06-18 14:05:13 +0200

    First Commit

---
+++ src/HelloFile.java
@@ -0,0 +1,34 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.Scanner;
+
+public class HelloFile {
+
+
+    public static void main(String[] args) throws IOException {
+        File file = new File("demo.txt");
+        Scanner scanner = new Scanner(file);
+        System.out.println("Now we are reading from " + file.getName());
+        String data;
+        while(scanner.hasNextLine()){
+            data= scanner.nextLine();
+            System.out.println(data);
+        }
+
+        write();
+
+
+
+    }
+
+    private static void write() throws IOException {
+        System.out.println("Hello File!");
+
+        FileWriter writer = new FileWriter("demo.txt", true);
+        writer.write(LocalDateTime.now()+"> Hello from Java\n");
+        writer.write("by your robot friend\n");
+        writer.close();
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,59 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello friend. \nWhat's your name?");
+        //creamos un objeto scanner
+        Scanner scanner = new Scanner(System.in);
+        //usamos el objeto scanner y lo parse a input
+        String input = scanner.nextLine();
+        if (input.toLowerCase().equals(" ")){
+            System.out.println("I don't like you "+ input);
+        }else{
+            System.out.println("Hello " + input);
+        }
+
+        int age;
+        do{
+            System.out.println("How old are you? ;) ");
+            try{age = scanner.nextInt();}
+            catch(Exception e){
+                age=0;
+                scanner=new Scanner(System.in);
+            }
+            if (age < 18) {
+               // System.err.println("You are very young!"); //err. sirve para que salga en rojo y prevalece con out y sale primero
+                System.err.println("Acces denied.");
+            }else{
+                System.out.println("You can join my friends club.");
+            }
+        }while(age<18);
+        scanner = new Scanner(System.in);
+        System.out.println("What's your favourite colour?");
+
+        switch (scanner.nextLine().trim().toUpperCase()){
+            case "RED" -> {
+                System.out.println("Fire walks with me");
+            }
+            case "BLUE" -> {
+                System.out.println("blue like a sea!");
+            }
+            case "GREEN" -> {
+                System.out.println("Money man! ");
+            }
+            case "YELLOW" -> {
+                System.out.println("Moliere hates you");
+            }
+            case "BLACK" -> {
+                System.out.println("Why so serious?");
+            }
+            default -> System.out.println("Oh you are so original");
+        }
+
+
+
+    }
+}
